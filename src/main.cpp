403639b2f6d8f275709019fe8b50e726611c8b56
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-23 23:22:23 -0700

    added, init arguement

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "init.h"
+#include "vcs.h"
+
 int main(int args, char* argv[]){
     if(args < 2){
         std::cerr << "no command typed \n";
         return 1;
+    }
+
+    std::string command = argv[1];
+
+    if(command == "init"){
+        init();
+    }else{
+        std::cerr << "unkown command \n";
     }
     
 }
