b1968ace221fdd19c7f940ebc908201a4a26e7c4
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-30 22:28:50 -0700

    added loop to add all arguements to vector, it is not neccesary now but will be in future

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include "init.h"
 #include "vcs.h"
+#include <unordered_map>
+#include <vector>
 
 int main(int args, char* argv[]){
     if(args < 2){
         std::cerr << "no command typed \n";
         return 1;
     }
+    std::vector<std::string> commands; 
 
-    std::string command = argv[1];
+   for(int i=1; i< args; i++){
+    commands.push_back(argv[i]);
+   }
+
+   
+
+    
 
     if(command == "init"){
         init();
+
     }else{
         std::cerr << "unkown command \n";
     }
